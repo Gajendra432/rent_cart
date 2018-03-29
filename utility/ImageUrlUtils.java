4e414405dfed7f950522ea7d92c3cd673f3dff1e
Gajendra432 <30731410+Gajendra432@users.noreply.github.com>
2018-03-29 12:48:19 +0530

    Add files via upload

---
+++ utility/ImageUrlUtils.java
@@ -0,0 +1,212 @@
+package com.allandroidprojects.ecomsample.utility;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 06peng on 2015/6/24.
+ */
+public class ImageUrlUtils {
+    static ArrayList<String> wishlistImageUri = new ArrayList<>();
+    static ArrayList<String> cartListImageUri = new ArrayList<>();
+
+    public static String[] getImageUrls() {
+        String[] urls = new String[] {
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+
+
+        };
+        return urls;
+    }
+
+    public static String[] getOffersUrls() {
+        String[] urls = new String[]{
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+
+        };
+        return urls;
+    }
+
+    public static String[] getHomeApplianceUrls() {
+        String[] urls = new String[]{
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+        };
+        return urls;
+    }
+
+    public static String[] getElectronicsUrls() {
+        String[] urls = new String[]{
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+        };
+        return urls;
+    }
+
+    public static String[] getLifeStyleUrls() {
+        String[] urls = new String[]{
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+        };
+        return urls;
+    }
+
+    public static String[] getBooksUrls() {
+        String[] urls = new String[]{
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+                "https://static.pexels.com/photos/53622/tractor-grain-mixer-rural-denmark-53622.jpeg",
+        };
+        return urls;
+    }
+
+    // Methods for Wishlist
+    public void addWishlistImageUri(String wishlistImageUri) {
+        this.wishlistImageUri.add(0,wishlistImageUri);
+    }
+
+    public void removeWishlistImageUri(int position) {
+        this.wishlistImageUri.remove(position);
+    }
+
+    public ArrayList<String> getWishlistImageUri(){ return this.wishlistImageUri; }
+
+    // Methods for Cart
+    public void addCartListImageUri(String wishlistImageUri) {
+        this.cartListImageUri.add(0,wishlistImageUri);
+    }
+
+    public void removeCartListImageUri(int position) {
+        this.cartListImageUri.remove(position);
+    }
+
+    public ArrayList<String> getCartListImageUri(){ return this.cartListImageUri; }
+}
